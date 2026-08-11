ce2f60163ae221efe09884d638daf592b8039cb8
Eddi Rendon <233111779@upmh.edu.mx>
2026-08-11 15:09:36 -0600

    cambios 2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, redirect, url_for
 import requests
 
@@ -37,4 +39,6 @@ def generar():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port= int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+    
